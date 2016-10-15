c0af1312978424a6997e0644829572d242b88902
Lluvio <caotousir@gmail.com>
2016-10-15 11:50:40 +0800

    upgrade less & fix theme

--- src/build.sh
+++ src/build.sh
@@ -8,11 +8,12 @@ mkdir -p _assets/website/
 mkdir -p _assets/ebook/
 
 # Compile Website CSS
-lessc -clean-css --include-path=$(node -e "console.log(require('gitbook-styleguide').less.paths.join(':'))") src/less/website.less _assets/website/style.css
+./node_modules/.bin/lessc -clean-css --include-path=$(node -e "console.log(require('gitbook-styleguide').less.paths.join(':'))") src/less/website.less _assets/website/style.css
 
 # Compile eBook CSS
-lessc -clean-css src/less/ebook.less _assets/ebook/ebook.css
+./node_modules/.bin/lessc -clean-css src/less/ebook.less _assets/ebook/ebook.css
 
 # Copy styleguide
-mkdir -p _assets/website
 cp -R node_modules/gitbook-styleguide/assets/ _assets/website/
+
+echo 'end'
